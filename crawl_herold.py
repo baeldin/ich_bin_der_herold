10f23e8022fdc76ff758a66be20e7e5abb37aa9c
phillip <phillip@justchaos.com>
2020-05-07 11:43:00 +0200

    loop and automization

--- crawl_herold.py
+++ crawl_herold.py
@@ -6,19 +6,22 @@ import re
 from HTMLParser import HTMLParser
 import codecs
 
-# PROXY shit
-# proxy = urllib2.ProxyHandler({
-#     'http': '54.37.21.29:8811',
-#     'https': '54.37.21.29:8811'})
-# opener = urllib2.build_opener(proxy)
-# urllib2.install_opener(opener)
-# print(urllib2)
-# res = urllib2.urlopen('https://www.whatsmyip.org/')
-# print(res)
-# print(res.read())
-# exit()
-
 htmlparser = HTMLParser()
+
+# BUNDESLÄNDERDATEN
+    # ä %C3%A4
+    # ö %C3%B6
+    # ü %C3%BC
+laender_daten = [
+        { "name": "wien",             "name_url": "wien" },
+        { "name": "niederösterreich", "name_url": "nieder%C3%B6sterreich" },
+        { "name": "oberösterreich",   "name_url": "ober%C3%B6sterreich" },
+        { "name": "salzburg",         "name_url": "salzburg" },
+        { "name": "steiermark",       "name_url": "steiermark" },
+        { "name": "tirol",            "name_url": "tirol" },
+        { "name": "vorarlberg",       "name_url": "vorarlberg" },
+        { "name": "kärnten",          "name_url": "k%C3%A4rnten" },
+        { "name": "burgenland",       "name_url": "burgenland" }]
 
 def make_url(word):
     # Word = unicode(word)
@@ -72,6 +75,17 @@ def get_zip(source_string):
         k += 1
 
 
+def get_npages(source_string):
+    string_list = re.split("[<>]", source_string)
+    k = 20
+    zip_list = []
+    for stri in string_list:
+        if stri == ' von ':
+            k = 0
+        if k == 2:
+            return int(stri)
+        k += 1
+
 def get_names(source_string):
     string_list = re.split("[<>]", source_string)
     k = 2
@@ -94,32 +108,42 @@ def get_mail(source_string):
 
 
 def main():
-    npages = 9
-    csvfile = open('hotels_niederösterreich0.csv','w')
-    wr = csv.writer(csvfile, delimiter=',')
-    wr.writerow(['name','PLZ','Ort','E-mail'])
-    # ä %C3%A4
-    # ö %C3%B6
-    # ü %C3%BC
-    for pp in range(1,14):
-    # for pp in range(14,npages+1):
-        url_string = "https://www.herold.at/gelbe-seiten/nieder%C3%B6sterreich/was_hotel/?page="+str(pp)
-        source_string = open_url(url_string)
-        url_list = get_entry_url_list(source_string)
-        name_list = get_names(source_string)
-        zip_list = get_zip(source_string)
-        for entry, name in zip(url_list, name_list):
-            source_string = open_url(entry)
-            mail = get_mail(source_string)
-            zip_code = get_zip(source_string)
-            town = get_town(source_string)
-            if mail is None:
-                print("omitting "+name+", no mail found")
-            else:
-                print(name,zip_code,town,mail)
-                # print("got: "+name+" "+zip_code+" "+town+" "+mail)
-                wr.writerow([name, zip_code, town, mail])
-            sleep(1)
+    trade = 'installateur'
+    for land in laender_daten:
+        urlstring = "https://www.herold.at/gelbe-seiten/{:s}/was_{:s}/".format(
+                land['name_url'], trade)
+        source_string = open_url(urlstring)
+        try:
+            npages = int(get_npages(source_string))
+        except:
+            npages = 1
+        csvfile_name = '{:s}_{:s}.csv'.format(
+            trade, land['name'])
+        csvfile = open(csvfile_name,'w')
+        wr = csv.writer(csvfile, delimiter=',')
+        wr.writerow(['name','PLZ','Ort','E-mail'])
+        print("Running for {:s} in {:s}, found {:d} pages. Writing results to {:s}.".format(
+            trade, land['name'], npages, csvfile_name))
+        for pp in range(1,npages+1):
+            urlstring = "https://www.herold.at/gelbe-seiten/{:s}/was_{:s}/?page={:d}".format(
+                land['name_url'], trade, pp)
+            print("Parsing page "+str(pp))
+            source_string = open_url(urlstring)
+            url_list = get_entry_url_list(source_string)
+            name_list = get_names(source_string)
+            zip_list = get_zip(source_string)
+            for entry, name in zip(url_list, name_list):
+                source_string = open_url(entry)
+                mail = get_mail(source_string)
+                zip_code = get_zip(source_string)
+                town = get_town(source_string)
+                if mail is None:
+                    print("omitting "+name+", no mail found")
+                else:
+                    print(name,zip_code,town,mail)
+                    # print("got: "+name+" "+zip_code+" "+town+" "+mail)
+                    wr.writerow([name, zip_code, town, mail])
+                sleep(2)
 
 
 if __name__ == "__main__":
